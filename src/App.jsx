17b8bdfb03ad58e049c737b280a2fbc4c8fe7974
Anikesh-Srivastav <anikeshsrivastab@gmail.com>
2025-07-02 02:06:49 +0530

    Implemented HeroSection + 3D model

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { Navbar } from "./sections/Navbar"
-import { Hero } from "./sections/Hero"
-import CombinedStarryBackground from "./components/UI/CombinedStarryBackground"
+import { Navbar } from './sections/Navbar'
+import { Hero } from './sections/Hero'
+import CombinedStarryBackground from './components/UI/CombinedStarryBackground'
 
- 
 export const App = () => {
-   return (
+  return (
     <>
-    {/* Background fixed and covers full viewport */}
-    <CombinedStarryBackground 
-       starDensity={0.0003}
-       minDelay={1500}
-       maxDelay={4000}
-       starColor="#33c2cc"  
-       trailColor="#57db96"
-    >
-
-    {/* Your app content */}
+      <CombinedStarryBackground
+        starDensity={0.0003}
+        minDelay={1500}
+        maxDelay={4000}
+        starColor="#33c2cc"
+        trailColor="#57db96"
+      >
         <div className="container mx-auto max-w-7xl relative z-10">
           <Navbar />
           <Hero />
         </div>
-    </CombinedStarryBackground>
-  </>
-   )
- }
+      </CombinedStarryBackground>
+    </>
+  )
+}
